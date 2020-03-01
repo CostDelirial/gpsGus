049e33bfac442b4ccb96bc337b545a8fba7dc65c
CostDelirial <costdelirial@MacBook-Pro-de-diana.local>
2020-03-01 12:51:53 -0600

    Primer Commit

---
+++ modelos/gps.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import mongooseUniqueValidator from 'mongoose-unique-validator';
+
+export interface IGps extends Document {
+   serie: string;
+   imei: string;
+    latitud: number;
+    longitud: number;
+    velocidad: number;
+    alarma: string;
+    status: string;
+}
+
+const gpsSchema: Schema = new Schema({
+    serie: { type: String, unique:true, required: [true, 'La serie de GPS es obligatorio']},
+    imei: { type: String, unique:true, required: [true, 'El IMEI del GPS es obligatorio'] },
+    latitud: { type: Number, required: [true, 'La latitud es requerida'] },
+    longitud: { type: Number, required: [ true, 'La longitud es requerida'] },
+    velocidad: { type: String, required: [ true, 'la velocidad es requerida'] },
+    alarma: { type: String, required: false },
+    status: { type: String, default: 'ACTIVO'}
+}, { collection: 'gps'} );
+
+gpsSchema.plugin(mongooseUniqueValidator, { message: '{PATH} debe ser unico' } );
+export default mongoose.model<IGps>('Gps', gpsSchema);
